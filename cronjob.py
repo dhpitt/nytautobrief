752912a0f9bd3aacf780518b474d57df93e4101d
dhpitt <david.h.pitt@gmail.com>
2023-04-24 12:50:01 -0700

    autoformatted w/black

--- cronjob.py
+++ cronjob.py
@@ -1,5 +1,6 @@
 # Set up the cron job to fetch the news every morning.
 
 from crontab import CronTab
-cron = CronTab(tab = """0 7 * * * poetry run python main.py""")
+
+cron = CronTab(tab="""0 7 * * * poetry run python main.py""")
 cron.write()
--- main.py
+++ main.py
@@ -7,24 +7,24 @@ from gmail_interface import send_email
 
 message_stub = """Dear David, \nHere's your morning briefing.\n"""
 
-sections = ['politics', 'science', 'business']
+sections = ["politics", "science", "business"]
 
 # Pull top stories in each section
 # and coerce them into email form.
 
 for sect in sections:
     summary = retrieve_and_summarize(section=sect, length=7)
-    if summary[-1] != '.':
-        summary += '.'
-    message_stub += f'\nIn {sect}:\n{summary}\n' 
+    if summary[-1] != ".":
+        summary += "."
+    message_stub += f"\nIn {sect}:\n{summary}\n"
 
-message_stub += '\n\nThat\'s all for today. Until tomorrow,\nAutoBrief'
+message_stub += "\n\nThat's all for today. Until tomorrow,\nAutoBrief"
 
 # Date the message for today
 todays_date = datetime.date.today()
-date_str = f'{todays_date.month}-{todays_date.day}-{todays_date.year}'
-subject_str = f'{todays_date.month}/{todays_date.day}: your morning briefing'
-message_fname = f'./messages/{date_str}.txt'
+date_str = f"{todays_date.month}-{todays_date.day}-{todays_date.year}"
+subject_str = f"{todays_date.month}/{todays_date.day}: your morning briefing"
+message_fname = f"./messages/{date_str}.txt"
 
 # Save it to the messages folder
 with open(message_fname, "w+") as f:
@@ -32,5 +32,4 @@ with open(message_fname, "w+") as f:
     f.close()
 
 # Pass message to gmail API and send it out!
-send_email(to='david.h.pitt@gmail.com', subject = subject_str, body = message_fname)
-
+send_email(to="david.h.pitt@gmail.com", subject=subject_str, body=message_fname)
